5972a332ac696add888a3beea40a15a72ac66553
Alexej <alexejdubow@gmail.com>
2020-06-07 20:10:41 +0300

    Updates

---
+++ precache-manifest.b8214ce028796f29f032084b15953bd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "891af6737a83ab963fe62f0b5be95995",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd5fd2df49067fd1426e",
+    "url": "/static/css/main.31d9d373.chunk.css"
+  },
+  {
+    "revision": "9c73cf35a2fe6045cd78",
+    "url": "/static/js/2.51ba912a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.51ba912a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd5fd2df49067fd1426e",
+    "url": "/static/js/main.4e99966c.chunk.js"
+  },
+  {
+    "revision": "a30524a0de09f04dbec4",
+    "url": "/static/js/runtime-main.a4fe0447.js"
+  }
+]);
